3920ef304d6c803753ddeb9f7843081e6524ee08
Victor <victorf.salinasb@gmail.com>
2026-06-11 21:02:52 -0400

    personaje con animaciones de movimiento

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,17 +2,29 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
+@onready var sprite = $Sprite2D
+
 func _physics_process(delta):
 	var direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-	if Input.is_action_pressed("ui_left"):
+		sprite.animation = "walk_right"
+		sprite.play()
+	elif Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("ui_down"):
+		sprite.animation = "walk_left"
+		sprite.play()
+	elif Input.is_action_pressed("ui_down"):
 		direction.y += 1
-	if Input.is_action_pressed("ui_up"):
+		sprite.animation = "walk_down"
+		sprite.play()
+	elif Input.is_action_pressed("ui_up"):
 		direction.y -= 1
+		sprite.animation = "walk_up"
+		sprite.play()
+	else:
+		sprite.stop()
 	
 	velocity = direction.normalized() * SPEED
 	move_and_slide()
